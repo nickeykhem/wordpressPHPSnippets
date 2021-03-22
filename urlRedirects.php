e74b11577361919dd95e1ab21b127741fa092397
Nickey <nike121@gmail.com>
2021-03-23 05:06:18 +0800

    Fixed typo

--- urlRedirects.php
+++ urlRedirects.php
@@ -2,6 +2,6 @@
 
 if($_SERVER['REQUEST_URI']=="/somedir/some_page_you_want_to_redirect")
 {
-wp_redirect( ‘http://mysite.com/mynewpost’, 301 );
+wp_redirect( 'http://mysite.com/mynewpost', 301 );
 exit;
 }
