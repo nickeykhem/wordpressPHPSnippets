dac854f637c3e1a4865228e0a774d46012a7ea32
Nickey <nike121@gmail.com>
2021-06-23 12:39:11 +0800

    Create change_choose_an_option_text.php

---
+++ WooCommerce Snippets/change_choose_an_option_text.php
@@ -0,0 +1,12 @@
+//Customise the dropdown 'choose an option'
+add_filter('woocommerce_dropdown_variation_attribute_options_args', 'wcvdc_dropdown_choice', 10);
+
+
+// Displays the custom "Choose an option" on the front end
+function custom_dropdown_choice( $args ){
+
+  $args['show_option_none'] = "Select an option:";
+
+  return $args;
+  
+}
