19156fde290456100cdbfb628f3ec6de0d828b5f
Nickey <nike121@gmail.com>
2022-04-01 16:57:14 +0800

    Create disableGutenberg.php

---
+++ disableGutenberg.php
@@ -0,0 +1,15 @@
+<?php
+
+// Disable Gutenberg on the back end.
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+// Disable Gutenberg for widgets.
+add_filter( 'use_widgets_blog_editor', '__return_false' );
+
+add_action( 'wp_enqueue_scripts', function() {
+    // Remove CSS on the front end.
+    wp_dequeue_style( 'wp-block-library' );
+
+    // Remove inline global CSS on the front end.
+    wp_dequeue_style( 'global-styles' );
+}, 20 );
